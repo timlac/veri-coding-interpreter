46994a56aca8c7971ba3541bf51765a8f46b9b0f
timlac <tim@lachmann.nu>
2023-08-28 19:07:56 +0200

    small fix

--- create_master_file.py
+++ create_master_file.py
@@ -17,28 +17,32 @@ def set_column_values(df, obj):
 
 record_metadatas = get_metadata_from_records()
 
-path = "/home/tim/work/nexa/nexa-opensmile-processing/files/out/verimind_egemaps_v2_lld"
+opensmile_files_directory = "/home/tim/Work/nexa/nexa-opensmile-processing/files/out"
 
 
 slices = []
 
-for file in os.listdir(path):
+for file in os.listdir(opensmile_files_directory):
     print("processing file: ", file)
 
-    if file.startswith("."):
+    if file.startswith(".gitkeep"):
         continue
     else:
         filename = Path(file).stem
 
     file_metadata = filename2metadata(filename)
 
+    print(file_metadata.participant)
+    print(type(file_metadata.participant))
+
     if file_metadata.participant in excluded_participants:
+        print("file {} is in excluded participants, skipping...".format(filename))
         continue
 
     for record_metadata in record_metadatas:
         if record_metadata == file_metadata:
 
-            df = pd.read_csv(os.path.join(path, file))
+            df = pd.read_csv(os.path.join(opensmile_files_directory, file))
 
             set_column_values(df, record_metadata)
 
@@ -46,7 +50,7 @@ for file in os.listdir(path):
 
 
 df = pd.concat(slices)
-df.to_csv("files/out/master.csv", index=False)
+df.to_csv("files/out/master_functionals.csv", index=False)
 
 
 
